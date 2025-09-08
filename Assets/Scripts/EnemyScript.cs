4cfabea7278eedbd3fe69a82c4527cb89f0810fc
Lord-Bladesworth <102436627+Lord-Bladesworth@users.noreply.github.com>
2025-09-08 19:07:29 +0800

    progress
    -enemy gameplay
    -simple chairs to the scene

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    Transform playertarget;
+    
+    public Action playerReachedAction; 
+
+    [SerializeField]
+    float MaxTargetDistance = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playertarget= GameObject.FindObjectOfType<PlayerController>().transform;  
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+
+    private void LateUpdate()
+    {
+        MoveTowardsPlayer();
+    }
+    Vector3 playerposition;
+    void MoveTowardsPlayer()
+    {
+        Debug.Log(Vector3.Distance(transform.position, playerposition));
+        playerposition = playertarget.position;
+
+        if (Vector3.Distance(transform.position, playerposition) > MaxTargetDistance)
+        {
+            playerposition = new Vector3(playerposition.x, 0, playerposition.z);
+            transform.position = Vector3.MoveTowards(transform.position, playertarget.position, 0.06f);
+        }
+        else
+        {
+            playerReachedAction();
+        }
+     }
+    void OnPlayerReached()
+    {
+        Debug.Log("player has been reached!");
+    }    
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,12 +6,11 @@ public class PlayerController : MonoBehaviour
 {
 
     CharacterController characterController;
-    float speed = 0.25f;
+    float speed = 0.11f;
 
     Vector2 _input;
 
-    float MoveX = 0;
-    float MoveZ = 0;
+
     [SerializeField]
     float Deadzone = 0.32f;
     private void Awake()
@@ -29,34 +28,33 @@ public class PlayerController : MonoBehaviour
     {
         
     }
-    void ReadInput()
-    {
-        _input = new Vector2(
-           Mathf.Abs( Input.GetAxisRaw("Horizontal")) > Deadzone ? Input.GetAxis("Horizontal") : 0,
-           Mathf.Abs( Input.GetAxisRaw("Vertical"))> Deadzone ? Input.GetAxis("Vertical") : 0);
-    }
+
 
     private void LateUpdate()
     {
         ReadInput();
-        //MoveX =Mathf.Abs( Input.GetAxis("Horizontal")) > Deadzone ?Mathf.Sign(Input.GetAxisRaw("Horizontal"))* speed : 0;
-        //MoveZ =Mathf.Abs( Input.GetAxis("Vertical")) > Deadzone ? Mathf.Sign(Input.GetAxisRaw("Vertical")) * speed : 0;
         ApplyMovement();
     
     }
+    void ReadInput()
+    {
+        _input = new Vector2(
+           Mathf.Abs(Input.GetAxisRaw("Horizontal")) > Deadzone ? Input.GetAxis("Horizontal") : 0,
+           Mathf.Abs(Input.GetAxisRaw("Vertical")) > Deadzone ? Input.GetAxis("Vertical") : 0);
+    }
+    float MoveX = 0;
+    float MoveZ = 0;
     void ApplyMovement()
     {
         MoveX = _input.x * speed;
         MoveZ = _input.y * speed;
-        Debug.Log(_input + "" + MoveX + "" + MoveZ);
         characterController.Move(new Vector3(MoveX, 0, MoveZ));
     }
+    float RotateY = 0;
     void ApplyRotation()
     {
-
+        //transform.rotation = new Vector3();
     }
-
-
     class FrameInput
     {
         public Vector2 move;
